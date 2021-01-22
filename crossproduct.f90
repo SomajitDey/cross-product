aa824b271b3bb7a15818a78e9824b44fe6aadb7b
Somajit Dey <somajit@users.sourceforge.net>
2021-01-22 20:31:40 +0530

    first-commit

---
+++ crossproduct.f90
@@ -0,0 +1,33 @@
+!About:
+!This program describes how cross-product of two vectors may be
+!obtained using an (inline) array function which optimizes for speed.
+!
+!Author: Somajit Dey <sdphys_rs@caluniv.ac.in> January 2021
+
+program crossproduct
+integer,dimension(3)::v1,v2,crossprod,expectation
+do
+    write(*,*)'Give 1st vector:'
+    read(*,*)v1
+    write(*,*)'Give 2nd vector:'
+    read(*,*)v2
+
+!My One-Liner:
+    crossprod=cshift((cshift(v1,-1)*v2-v1*cshift(v2,-1)),-1)
+
+    write(*,*)'The cross-product is:'
+    write(*,'(3(I0,1X))')crossprod
+    
+!Conventional Determination of CrossProduct:
+    expectation(1)=v1(2)*v2(3)-v1(3)*v2(2)
+    expectation(2)=v1(3)*v2(1)-v1(1)*v2(3)
+    expectation(3)=v1(1)*v2(2)-v1(2)*v2(1)
+
+!Check if my one-liner gives the same result as expectation    
+    if(any(crossprod/=expectation))STOP 'Oops'
+
+    write(*,*)
+    write(*,*)'Press ENTER to continue'
+    read(*,*)
+enddo    
+end program crossproduct
